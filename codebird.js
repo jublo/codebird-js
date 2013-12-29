10f1b355af9625f64f521454dc0a74b6279d8d8e
J.M <me@mynetx.net>
2013-12-29 16:38:05 +0100

    Rename _base64_encode helper to indicate private visibility

--- codebird.js
+++ codebird.js
@@ -502,7 +502,7 @@ var Codebird = function () {
         xml.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         xml.setRequestHeader(
             (_use_proxy ? "X-" : "") + "Authorization",
-            "Basic " + base64_encode(_oauth_consumer_key + ":" + _oauth_consumer_secret)
+            "Basic " + _base64_encode(_oauth_consumer_key + ":" + _oauth_consumer_secret)
         );
 
         xml.onreadystatechange = function () {
@@ -644,53 +644,44 @@ var Codebird = function () {
         };
     }();
 
-    var base64_encode = function (data) {
-        // http://kevin.vanzonneveld.net
-        // +   original by: Tyler Akins (http://rumkin.com)
-        // +   improved by: Bayron Guevara
-        // +   improved by: Thunder.m
-        // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-        // +   bugfixed by: Pellentesque Malesuada
-        // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-        // +   improved by: Rafał Kukawski (http://kukawski.pl)
-        // *     example 1: base64_encode('Kevin van Zonneveld');
-        // *     returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
-        // mozilla has this native
-        // - but breaks in 2.0.0.12!
-        //if (typeof this.window['btoa'] == 'function') {
-        //    return btoa(data);
-        //}
-        var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-        var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
-            ac = 0,
-            enc = "",
-            tmp_arr = [];
-
-        if (! data) {
-            return data;
+    /*
+     * Gets the base64 representation for the given data
+     *
+     * http://phpjs.org
+     * +   original by: Tyler Akins (http://rumkin.com)
+     * +   improved by: Bayron Guevara
+     * +   improved by: Thunder.m
+     * +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+     * +   bugfixed by: Pellentesque Malesuada
+     * +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+     * +   improved by: Rafał Kukawski (http://kukawski.pl)
+     *
+     * @param string data The data to calculate the base64 representation from
+     *
+     * @return string The base64 representation
+     */
+    var _base64_encode = function (a) {
+        var d, e, f, b, g = 0,
+            h = 0,
+            i = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+            c = [];
+        if (!a) {
+            return a;
         }
-
-        do { // pack three octets into four hexets
-            o1 = data.charCodeAt(i++);
-            o2 = data.charCodeAt(i++);
-            o3 = data.charCodeAt(i++);
-
-            bits = o1 << 16 | o2 << 8 | o3;
-
-            h1 = bits >> 18 & 0x3f;
-            h2 = bits >> 12 & 0x3f;
-            h3 = bits >> 6 & 0x3f;
-            h4 = bits & 0x3f;
-
-            // use hexets to index into b64, and append result to encoded string
-            tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
-        } while (i < data.length);
-
-        enc = tmp_arr.join("");
-
-        var r = data.length % 3;
-
-        return (r ? enc.slice(0, r - 3) : enc) + "===".slice(r || 3);
+        do {
+            d = a.charCodeAt(g++);
+            e = a.charCodeAt(g++);
+            f = a.charCodeAt(g++);
+            b = d << 16 | e << 8 | f;
+            d = b >> 18 & 63;
+            e = b >> 12 & 63;
+            f = b >> 6 & 63;
+            b &= 63;
+            c[h++] = i.charAt(d) + i.charAt(e) + i.charAt(f) + i.charAt(b);
+        } while (g < a.length);
+        c = c.join("");
+        a = a.length % 3;
+        return (a ? c.slice(0, a - 3) : c) + "===".slice(a || 3);
     };
 
     var http_build_query = function (formdata, numeric_prefix, arg_separator) {
