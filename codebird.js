4a85eaeab79f53fd1487e4d842740a1c8ce464d6
J.M <jm@jublo.net>
2014-06-18 18:41:36 +0200

    Update version to 2.5.0-rc.3-dev

--- codebird.js
+++ codebird.js
@@ -2,7 +2,7 @@
  * A Twitter library in JavaScript
  *
  * @package codebird
- * @version 2.5.0-rc.2
+ * @version 2.5.0-rc.3-dev
  * @author Jublo Solutions <support@jublo.net>
  * @copyright 2010-2014 Jublo Solutions <support@jublo.net>
  *
@@ -146,7 +146,7 @@ var Codebird = function () {
     /**
      * The current Codebird version
      */
-    var _version = "2.5.0-rc.2";
+    var _version = "2.5.0-rc.3-dev";
 
     /**
      * Sets the OAuth consumer key and secret (App key)
