a6b7b85eb32d712b38136711c3bdadc566848d45
J.M <jm@jublo.net>
2014-06-21 20:06:35 +0200

    Simpler PHPDoc file header for codebird.js

--- codebird.js
+++ codebird.js
@@ -1,23 +1,12 @@
 /**
  * A Twitter library in JavaScript
  *
- * @package codebird
- * @version 2.5.0-rc.3-dev
- * @author Jublo Solutions <support@jublo.net>
+ * @package   codebird
+ * @version   2.5.0-rc.3-dev
+ * @author    Jublo Solutions <support@jublo.net>
  * @copyright 2010-2014 Jublo Solutions <support@jublo.net>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * @license   http://opensource.org/licenses/GPL-3.0 GNU Public License 3.0
+ * @link      https://github.com/jublonet/codebird-php
  */
 
 /* jshint curly: true,
