db104896d4d0ba679f247b0af51ce7a765ef9b2e
J.M <jm@jublo.net>
2015-04-08 10:45:08 +0200

    [TASK] Set version to 2.6.0

--- codebird.js
+++ codebird.js
@@ -2,7 +2,7 @@
  * A Twitter library in JavaScript
  *
  * @package   codebird
- * @version   2.6.0-rc.1
+ * @version   2.6.0
  * @author    Jublo Solutions <support@jublo.net>
  * @copyright 2010-2015 Jublo Solutions <support@jublo.net>
  * @license   http://opensource.org/licenses/GPL-3.0 GNU Public License 3.0
@@ -135,7 +135,7 @@ var Codebird = function () {
     /**
      * The current Codebird version
      */
-    var _version = "2.6.0-rc.1";
+    var _version = "2.6.0";
 
     /**
      * Sets the OAuth consumer key and secret (App key)
