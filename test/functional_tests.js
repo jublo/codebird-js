ecf24a34541f8c027129bd957465adeab6028143
J.M <jm@jublo.net>
2018-09-15 13:42:19 +0200

    Add reply XML detection

--- test/functional_tests.js
+++ test/functional_tests.js
@@ -1,37 +1,60 @@
 const tape = require("tape"),
   _test = require("tape-promise"),
   test = _test(tape), // decorate tape
-  CodebirdT = require("./codebirdt");
+  CodebirdT = require("./codebirdt"),
+  CodebirdM = require("./codebirdm");
 
-function getCB() {
-  var cb = new CodebirdT.default;
+function getCB(mock) {
+  if (typeof mock === "undefined") {
+    var mock = false;
+  }
+  var cb = mock ? new CodebirdM.default() : new CodebirdT.default();
   cb.setConsumerKey("123", "456");
 
   return cb;
 }
 
-test("Tests statuses/update", function (t) {
+test("Tests statuses/update", function(t) {
   const cb = getCB();
   t.plan(1);
 
   cb.setToken("123", "456");
-  return cb.__call(
-    "statuses_update",
-    {"status": "Whohoo, I just tweeted!"}
-  ).then(function (reply, rate, err) {
-    t.deepEqual(
-      reply,
-      {
+  return cb
+    .__call("statuses_update", { status: "Whohoo, I just tweeted!" })
+    .then(function(reply, rate, err) {
+      t.deepEqual(reply, {
         rate: {
-          limit: null, remaining: null, reset: null
+          limit: null,
+          remaining: null,
+          reset: null
         },
         reply: {
-          errors: [
-            { code: 89, message: "Invalid or expired token." }
-          ],
+          errors: [{ code: 89, message: "Invalid or expired token." }],
           httpstatus: 401
         }
-      }
-    )
-  });
+      });
+    });
+});
+
+test("Tests reply XML detection", function(t) {
+  const cb = getCB(true);
+  t.plan(1);
+
+  cb.setToken("123", "456");
+  return cb
+    .__call("oauth_requestToken", {
+      oauth_callback: "http://www.example.com/#xml_detection"
+    })
+    .then(function(reply, rate, err) {
+      t.deepEqual(reply, {
+        reply: {
+          errors: {
+            "415":
+              "Callback URL not approved for this client application. Approved callback URLs can be adjusted in your application settings"
+          },
+          httpstatus: 401
+        },
+        rate: null
+      });
+    });
 });
