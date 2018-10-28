873ca866c2f1400bcabd4427b68e05be886c8025
J.M <jm@jublo.net>
2018-10-28 15:58:41 +0100

    Increase test listener count

--- test/oauth_tests.js
+++ test/oauth_tests.js
@@ -1,4 +1,4 @@
-require("events").EventEmitter.defaultMaxListeners = 15;
+require("events").EventEmitter.defaultMaxListeners = 50;
 
 const tape = require("tape"),
   _test = require("tape-promise"),
