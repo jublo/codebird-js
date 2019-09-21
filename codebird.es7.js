d1bf8b9f584d8ac30289f63904f7b71d78575559
Eugenio Valeiras <evaleiras@clapps.xyz>
2019-09-20 21:35:58 -0300

    Fix Media upload.

--- codebird.es7.js
+++ codebird.es7.js
@@ -1553,7 +1553,7 @@
           params = this._http_build_query(params);
         }
         post_fields = params;
-        if (this._use_proxy || multipart) {
+        if (this._use_proxy) {
           // force proxy for multipart base64
           url = url
             .replace(this._endpoint_base, this._endpoint_proxy)
